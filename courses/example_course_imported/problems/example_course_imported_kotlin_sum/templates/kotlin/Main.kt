b227996140f69c32ce76f9ab4aec0b878321f0d0
ponharu <128346975+ponharu@users.noreply.github.com>
2026-08-25 13:57:08 +0900

    fix: make Kotlin and Android judging examples usable

---
+++ courses/example_course_imported/problems/example_course_imported_kotlin_sum/templates/kotlin/Main.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val count = scanner.nextInt()
+    var total = 0L
+    for (index in 0 until count) {
+        val value = scanner.nextInt()
+        // TODO: valueをtotalに加えてください
+    }
+    println(total)
+    scanner.close()
+}
